93e3a745404f2f66edea92670c5adcaf6b8770c0
Gonzalo Fiz Pontiveros <g.fiz.pontiveros@gmail.com>
2020-05-27 21:56:40 +0200

    Simplified code for enviroment step.

--- gym_snake/envs/snake_env.py
+++ gym_snake/envs/snake_env.py
@@ -3,32 +3,58 @@ import numpy as np
 from gym_snake.envs.snake_game import SnakeGame
 from gym import error, spaces, utils
 from gym.utils import seeding
-from gym.spaces import Discrete
+from gym.spaces import Discrete, Box
 
 class SnakeEnv(gym.Env):
-  metadata = {'render.modes': ['human']}
-  def __init__(self,height,width):
-      self.game_object = SnakeGame(height, width)
-      self.game_object.insert_pill()
-      self.action_space = Discrete(4)
-      self.observation_space = self.game_object.observation()
-  def step(self, a):
-      game = self.game_object
-      action = game.ACTIONS[a]
-      old_score = game.score
-      game.step(action)
-      return game.observation(), old_score-game.score, game.active_game
-  def reset(self):
-      self.game_object.reset()
-      return self.game_object.observation()
-  def render(self, mode='human'):
-    game = self.game_object
-    if mode == 'human':
-        board = np.zeros(game.board_shape)
-        board[:,:] = game.board
-        head = game.snake[-1]
-        board[head[0], head[1]] = 3
-        print(board)
-    ...
-  def close(self):
-    ...
+    metadata = {'render.modes': ['human', 'rgb_array']}
+
+    def __init__(self, height, width):
+        self.game_object = SnakeGame(height, width)
+        self.game_object.insert_pill()
+        self.action_space = Discrete(4)
+        self.observation_space = Box(
+            low=-1, high=1, shape=self.game_object.observation().shape)
+
+    def get_keys_to_action(self):
+        keys_to_action = {(ord('w'),):0, (ord('s'),): 1, (ord('a'),): 2, (ord('d'),): 3}
+        return keys_to_action
+
+    def step(self, a):
+        game = self.game_object
+        action = game.ACTIONS[a]
+        old_score = game.score
+        game.step(action)
+        info = {'ACTION_NAME': action}
+        return game.observation(), old_score-game.score, not game.active_game, info
+
+    def reset(self):
+        self.game_object.reset()
+        return self.game_object.observation()
+
+    def render(self, mode='human'):
+        game = self.game_object
+        if mode == 'human':
+            board = np.zeros(game.board_shape)
+            board[:, :] = game.board
+            head = game.snake.body[-1]
+            board[head[0], head[1]] = 2
+            print('Score: {}'.format(game.score))
+            print(board)
+        if mode == 'rgb_array':
+            height, width = game.board_shape
+            board_rgb = np.zeros((height, width, 3))
+            head = game.snake.body[-1]
+            for i in range(height):
+                for j in range(width):
+                    if game.board[i, j] == -1:
+                        board_rgb[i, j, :] = [255, 0, 0]
+                    if game.board[i, j] == 0:
+                        board_rgb[i, j, :] = [0, 255, 0]
+                    if game.board[i, j] == 1:
+                        board_rgb[i, j,:] = [0, 0, 255]
+                    if i == head[0] and j == head[1]:
+                        board_rgb[i, j,:] = [255, 255, 0]
+            return board_rgb.astype(np.uint8)
+
+    def close(self):
+        ...
--- gym_snake/envs/snake_game.py
+++ gym_snake/envs/snake_game.py
@@ -1,15 +1,41 @@
+from gym.spaces import Box
 import numpy as np
 import random
 import os
 
 
+class Snake:
+    def __init__(self, start):
+        self.body = [start]
+
+    def move(self, action):
+        head = self.body[-1]
+        head_x, head_y = head[0], head[1]
+        if action == 'UP':
+            new_head = [head_x-1, head_y]
+            self.body.append(new_head)
+            self.body.pop(0)
+        if action == 'DOWN':
+            new_head = [head_x+1, head_y]
+            self.body.append(new_head)
+            self.body.pop(0)
+        if action == 'LEFT':
+            new_head = [head_x, head_y-1]
+            self.body.append(new_head)
+            self.body.pop(0)
+        if action == 'RIGHT':
+            new_head = [head_x, head_y+1]
+            self.body.append(new_head)
+            self.body.pop(0)
+        
 
 
 class SnakeGame:
     """Snake game representation.  
     """
-    ACTIONS = ['UP', 'DOWN', 'LEFT', 'RIGHT']    
-    def __init__(self, height, width,start=None):
+    ACTIONS = ['UP', 'DOWN', 'LEFT', 'RIGHT']
+
+    def __init__(self, height: int, width: int, start=None):
         """Initialize SnakeGame on a a board of given size with a start postion. A zero in the matrix indicates a "free position", a "-1" indicates a forbidden postion and "1" will indicate a pill position.
 
         Args:
@@ -18,85 +44,38 @@ class SnakeGame:
             height (int): Height of board
         """
         if start == None:
-            start = [random.randint(1, height - 2), random.randint(1, width - 2)]
-            
-        self.board_shape = (height,width)
+            start = [random.randint(1, height - 2),
+                     random.randint(1, width - 2)]
+
+        self.board_shape = (height, width)
         self.score = 0
-        self.snake = [start]
+        self.snake = Snake(start)
         self.board = np.zeros((height, width))
-        self.board[0,:] = -np.ones(width)
-        self.board[height - 1,:] = -np.ones(width)
+        self.board[0, :] = -np.ones(width)
+        self.board[height - 1, :] = -np.ones(width)
         self.board[:, 0] = -np.ones(height)
         self.board[:, width - 1] = -np.ones(height)
         self.board[start[0], start[1]] = -1
         self.active_game = True
 
     def step(self, action):
-        head = self.snake[-1]
-        head_x, head_y = head[0], head[1]
-        if action == 'UP':
-            new_head = [head_x-1,head_y]
-            value = self.board[head_x-1,head_y]
-            self.score += value
-            if value == 0:
-                tail = self.snake.pop(0)
-                self.snake.append(new_head)
-                self.board[tail[0], tail[1]] = 0
-                self.board[new_head[0], new_head[1]] = -1
-            if value == -1:
-                self.active_game = False
-            if value == 1:
-                self.board[new_head[0], new_head[1]] = -1
-                self.snake.append(new_head)
-                self.insert_pill()
-        if action == 'DOWN':
-            new_head = [head_x+1,head_y]
-            value = self.board[head_x+1,head_y]
-            self.score += value
-            if value == 0:
-                tail = self.snake.pop(0)
-                self.snake.append(new_head)
-                self.board[tail[0], tail[1]] = 0
-                self.board[new_head[0], new_head[1]] = -1
-            if value == -1:
-                self.active_game = False
-            if value == 1:
-                self.board[new_head[0], new_head[1]] = -1
-                self.snake.append(new_head)
-                self.insert_pill()
-        if action == 'RIGHT':
-            new_head = [head_x,head_y+1]
-            value = self.board[head_x,head_y+1]
-            self.score += value
-            if value == 0:
-                tail = self.snake.pop(0)
-                self.snake.append(new_head)
-                self.board[tail[0], tail[1]] = 0
-                self.board[new_head[0], new_head[1]] = -1
-            if value == -1:
-                self.active_game = False
-            if value == 1:
-                self.board[new_head[0], new_head[1]] = -1
-                self.snake.append(new_head)
-                self.insert_pill()
-        if action == 'LEFT':
-            new_head = [head_x,head_y-1]
-            value = self.board[head_x,head_y-1]
-            self.score += value
-            if value == 0:
-                tail = self.snake.pop(0)
-                self.snake.append(new_head)
-                self.board[tail[0], tail[1]] = 0
-                self.board[new_head[0], new_head[1]] = -1
-            if value == -1:
-                self.active_game = False
-            if value == 1:
-                self.board[new_head[0], new_head[1]] = -1
-                self.snake.append(new_head)
-                self.insert_pill()
-
+        tail = self.snake.body[0]
+        self.snake.move(action)
+        head = self.snake.body[-1]
+        reward = self.board[head[0], head[1]]
+        self.score += reward
+        if reward == 0:
+            self.board[tail[0], tail[1]] = 0
+            self.board[head[0], head[1]] = -1
+        if reward == -1:
+            self.active_game = False
+        if reward == 1:
+            self.board[head[0], head[1]] = -1
+            self.snake.body.insert(0,tail)
+            self.insert_pill()
+    
     def insert_pill(self):
-        height,width = self.board_shape
+        height, width = self.board_shape
         board = self.board
         if np.any(board == 0):
             x = random.randint(0, height-1)
@@ -107,11 +86,15 @@ class SnakeGame:
             board[x, y] = 1
         else:
             self.active_game = False
+
     def reset(self):
         self.__init__(*self.board_shape)
-        self.insert_pill()            
-    
-    def observation(self):
-        raise NotImplementedError
+        self.insert_pill()
 
-        
+    def observation(self):
+        board_features = self.board
+        head = self.snake.body[-1]
+        head_features = np.zeros(self.board_shape)
+        head_features[head[0], head[1]] = 1
+        combined_features = np.concatenate((board_features, head_features))
+        return combined_features
